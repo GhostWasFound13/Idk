3b3ccc5b9af390bd841170486a200e94635ac896
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-01 19:49:09 +0700

    Update type.ts

--- src/type.ts
+++ src/type.ts
@@ -10,3 +10,41 @@ export interface Message {
   content: string;
   author: User;
 }
+// ... (existing types)
+
+export interface Embed {
+  title?: string;
+  description?: string;
+  url?: string;
+  color?: number;
+  fields?: EmbedField[];
+  footer?: EmbedFooter;
+}
+
+export interface EmbedField {
+  name: string;
+  value: string;
+  inline?: boolean;
+}
+
+export interface EmbedFooter {
+  text: string;
+  icon_url?: string;
+}
+
+export interface Attachment {
+  name: string;
+  content: Buffer | string;
+}
+
+export interface AvatarOptions {
+  avatar: Buffer | string;
+}
+
+export interface BannerColorOptions {
+  color: number;
+}
+
+export interface IntentsOptions {
+  all: boolean;
+}
