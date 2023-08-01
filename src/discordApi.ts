1416d4065dbf7c1ec5615e049d81820ab9efe1b2
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-01 19:51:52 +0700

    Update discordApi.ts

--- src/discordApi.ts
+++ src/discordApi.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { WebSocket } from 'ws';
-import { User, Message } from './types';
-
+import { Attachment, AvatarOptions, BannerColorOptions, Embed, IntentsOptions } from './types';
 interface DiscordApiOptions {
   token: string;
 }
@@ -87,6 +86,112 @@ class DiscordApi {
       throw new Error('Failed to send the message.');
     }
   }
+async sendMessageWithEmbed(channelId: string, content: string, embed: Embed): Promise<Message> {
+    try {
+      const { data } = await axios.post<Message>(
+        `${this.apiBaseUrl}/channels/${channelId}/messages`,
+        {
+          content: content,
+          embed: embed,
+        },
+        {
+          headers: {
+            Authorization: `Bot ${this.token}`,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      throw new Error('Failed to send the message with the embed.');
+    }
+  }
+
+  async sendMessageWithAttachment(channelId: string, content: string, attachment: Attachment): Promise<Message> {
+    try {
+      const { data } = await axios.post<Message>(
+        `${this.apiBaseUrl}/channels/${channelId}/messages`,
+        {
+          content: content,
+        },
+        {
+          headers: {
+            Authorization: `Bot ${this.token}`,
+          },
+          files: [
+            {
+              name: attachment.name,
+              attachment: attachment.content,
+            },
+          ],
+        }
+      );
+      return data;
+    } catch (error) {
+      throw new Error('Failed to send the message with the attachment.');
+    }
+  }
+
+  async updateAvatar(options: AvatarOptions): Promise<User> {
+    try {
+      const { data } = await axios.patch<User>(
+        `${this.apiBaseUrl}/users/@me`,
+        {
+          avatar: options.avatar,
+        },
+        {
+          headers: {
+            Authorization: `Bot ${this.token}`,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      throw new Error('Failed to update the avatar.');
+    }
+  }
+
+  async updateBannerColor(options: BannerColorOptions): Promise<User> {
+    try {
+      const { data } = await axios.patch<User>(
+        `${this.apiBaseUrl}/users/@me`,
+        {
+          banner_color: options.color,
+        },
+        {
+          headers: {
+            Authorization: `Bot ${this.token}`,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      throw new Error('Failed to update the banner color.');
+    }
+  }
+
+  async enableAllIntents(options: IntentsOptions): Promise<void> {
+    try {
+      if (options.all) {
+        const allIntents = 32767; // Sum of all intent flags
+        const { data } = await axios.patch(
+          `${this.apiBaseUrl}/applications/@me/bot`,
+          {
+            intents: allIntents,
+          },
+          {
+            headers: {
+              Authorization: `Bot ${this.token}`,
+            },
+          }
+        );
+        console.log('Enabled all intents.');
+      }
+    } catch (error) {
+      throw new Error('Failed to enable all intents.');
+    }
+  }
+
+  
 
   // Add more methods for interacting with the Discord API here
 }
